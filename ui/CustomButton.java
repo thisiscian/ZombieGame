6a29963cd6bc908e0b972976d71c5eac24f19458
thisiscian <thisiscian@gmail.com>
2012-10-05 20:26:09 +0100

    better fullscreen, worse keybindings

--- ui/CustomButton.java
+++ ui/CustomButton.java
@@ -2,6 +2,7 @@ package ui;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import ui.FullScreen;
 
 import java.util.ArrayList;
 
@@ -9,40 +10,37 @@ public class CustomButton extends JComponent implements MouseListener
 {
 	private Dimension size = new Dimension(30,30);
 	private ArrayList<ActionListener> listeners = new ArrayList<ActionListener>();
-	private boolean pressed = false;
+	private boolean isPressed = false;
 	private boolean mouseEntered = false;
-	private boolean toggle = false;
 	private char txt = '1';
 
 	public Dimension arc = new Dimension((int)Math.sqrt(size.width), (int)Math.sqrt(size.height));
 
-	public CustomButton(char text, boolean tog)
+	public CustomButton(char text, FullScreen screen)
 	{
-		super();
-		txt = text;
-		toggle = tog;
-		enableInputMethods(true);
-		addMouseListener(this);
-		setFocusable(true);
-		requestFocusInWindow();	
+		setValues(text);
+		screen.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.getExtendedKeyCodeForChar(text), 0, false), "pressed");
+    screen.getActionMap().put("pressed", pressed);
+		screen.add(this);
 	}
 
-	public CustomButton(char text)
+	private void setValues(char text)
 	{
-		super();
 		txt = text;
-		enableInputMethods(true);
-		addMouseListener(this);
-		setFocusable(true);
-	}
-
-	public CustomButton()
+		this.enableInputMethods(true);
+		this.addMouseListener(this);
+		this.setFocusable(true);
+ 	}
+	
+	private Action pressed = new AbstractAction("pressed")
 	{
-		super();
-		enableInputMethods(true);
-		addMouseListener(this);
-		setFocusable(true);
-	}
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			isPressed = !isPressed;
+			repaint();
+		}
+  };
 
 	public void paintComponent(Graphics g)
 	{
@@ -57,7 +55,7 @@ public class CustomButton extends JComponent implements MouseListener
 			g.setColor(Color.GRAY);
 			g.fillRoundRect(0,0,getWidth(),getHeight(), arc.width,arc.height);
 		}
-		if(pressed)
+		if(isPressed)
 		{
 			g.setColor(Color.decode("#250000"));
 		}
@@ -67,7 +65,7 @@ public class CustomButton extends JComponent implements MouseListener
 		}
 		g.fillRoundRect(1,1,getWidth()-2,getHeight()-2, arc.width, arc.height);
 		g.setColor(Color.WHITE);
-		if(pressed)
+		if(isPressed)
 		{
 			g.drawString("R", getWidth()-3-metric.stringWidth("R"), getHeight()-3-metric.getHeight());
 		}
@@ -91,40 +89,17 @@ public class CustomButton extends JComponent implements MouseListener
 		return getPreferredSize();
 	}
 
-	public void pressed()
-	{
-		if(toggle)
-		{
-			pressed = !pressed;
-		}
-		else
-		{
-			pressed = true;
-		}
-		repaint();
-	}
-	
-	public void released()
-	{
-		if(!toggle) pressed = false;
-		repaint();
-	}
-	
 	public void mouseClicked(MouseEvent e){}
 	public void mousePressed(MouseEvent e)
 	{
 		notifyListeners(e);
-		if(toggle){pressed = !pressed;}
-		else{pressed = true;}
+		isPressed = true;
 		repaint();
 	}
 	public void mouseReleased(MouseEvent e)
 	{
-		if(!toggle)
-		{
-			pressed = false;
+			isPressed = false;
 			repaint();
-		}
 	}
 	public void mouseExited(MouseEvent e)
 	{
